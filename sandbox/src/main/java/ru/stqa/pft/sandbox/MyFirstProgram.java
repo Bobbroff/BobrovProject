4d67f5517172395d881ce09c62c78f81d1c7cd82
Bobbroff <mifkabob@yandex.ru>
2021-08-01 18:08:57 +0300

    Доработка задания №2 (Point class)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,14 +13,10 @@ public class MyFirstProgram {
 	    Rectangle r = new Rectangle(4, 6);
 	    System.out.println("Площадь прямугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-	    Point p1 = new Point(10,20);
-	    Point p2 = new Point(5,1);
-	    System.out.println("Растояние между точками на плоскости = " + distance(p1,p2));
+	    Point p1 = new Point(1, 5);
+	    Point p2 = new Point(10, 10);
+	    System.out.println("Расстояние между точками: " + p1.distance(p2));
     }
-
-	public static double distance (Point p1, Point p2){
-		return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
-	}
 
     public static void hello(String somebody){
 		System.out.println("Hello, " + somebody + "!");
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,16 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
+    private double x;
+    private double y;
 
-    public double x;
-    public double y;
-
-
-    public Point(double x, double y){
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
-
     }
 
-
+    public double distance(Point p2) {
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+    }
 }
